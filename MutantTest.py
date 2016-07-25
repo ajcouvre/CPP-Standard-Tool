03fe27efc5eef170becac003106d9f4feffcf4f8
ajcouvre <ajcouvre@gmail.com>
2016-07-25 15:49:36 -0700

    file open fix

--- MutantTest.py
+++ MutantTest.py
@@ -79,7 +79,7 @@ def create_mutants(OriginalDict):
 def write_files(MutantDict):
 	for name, example in MutantDict.iteritems():
 		for mutant, lines in example:
-			newfile = open(name + mutant + ".cpp")
+			newfile = open(name + mutant + ".cpp", 'w')
 			for line in lines:
 				newFile.write(line)
 				newfile.write("\n")

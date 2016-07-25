6271c1abbe60f25717d78e7dc560bb640b856928
AJ <aj@vule.cs.ucdavis.edu>
2016-07-25 15:59:43 -0700

    single page range

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import subprocess
 
 inFile = raw_input("Input the PDF name to turn to text: ")
 pageRangeStart = raw_input("Please enter the page start, -1 for all:")
-pageRangeEnd = raw_input("Please enter the page end, -1 for all:")
+pageRangeEnd = raw_input("Please enter the page end, -1 for all, same page for single page:")
 outFile = raw_input("Name of the output file: ")
 pageRangeStart = int(pageRangeStart)
 pageRangeEnd = int(pageRangeEnd)
 
 if pageRangeStart == -1 and pageRangeEnd == -1:
 	proc = subprocess.Popen(['python', 'pdf2txt.py', '-o', outFile, inFile])
+elif pageRangeStart == pageRangeEnd:
+	proc = subprocess.Popen(['python', 'pdf2txt.py', '-p', pageRangeStart, '-o', outFile, inFile])
 else:
 	page_list = [str(x) for x in range(pageRangeStart, pageRangeEnd+1)]
 	page_string = ', '.join(page_list)

a6a665f7c5fe8ee48634f820d169b43c67a642f6
ajcouvre <ajcouvre@gmail.com>
2016-08-09 17:35:58 -0700

    CPP All Headers

---
+++ CPP_Headers.h
@@ -0,0 +1,147 @@
+//Utilities Library
+#include <cstdlib>
+#include <csginal>
+#include <csetjump>
+#include <cstdarg>
+#include <typeinfo>
+#include <typeindex>
+#include <type_traits>
+#include <bitset>
+#include <fucntional>
+#include <utility>
+#include <ctime>
+#include <chrono>
+#include <cstddef>
+#include <initializer_list>
+#include <tuple>
+#include <any>
+#include <optional>
+#include <variant>
+
+//Dynamic Memory Management
+#include <new>
+#include <memory>
+#include <scoped_allocator>
+#include <memory_resource>
+
+//Numeric Limits
+#include <climits>
+#include <cfloat>
+#include <cstdint>
+#include <cinttypes>
+#include <limits>
+
+//Error Handling
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <system_error>
+#include <cerrno>
+
+//Strings Library
+#include <cctype>
+#include <cwctype>
+#include <cstring>
+#include <cwchar>
+#include <cuchar>
+#include <string>
+#include <string_view>
+
+//Containers Library
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+
+//Algorithms Library
+#include <algorithm>
+#include <execution>
+
+//Iterators library
+ <iterator>
+
+//Numerics Library
+#include <cmath>
+#include <complex>
+#include <valarray>
+#include <random>
+#include <numeric>
+#include <ratio>
+#include <cfenv>
+
+//Input/Output Library
+#include <iosfwd>
+#include <ios>
+#include <istream>
+#include <ostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <strstream>
+#include <iomanip>
+#include <streambuf>
+#include <cstdtio>
+
+//Localization Library
+#include <locale>
+#include <clocale>
+#include <codecvt>
+
+//Regular Expressions Library
+#include <regex>
+
+//Atomic Operations Library
+#include <atomic>
+
+//Thread Support Library
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <future>
+#include <condition_variable>
+
+//Filesystem Library
+#include <filesystem>
+
+//C Compatibility Headers
+#include <ciso646>
+#include <ccomplex>
+#include <ctgmath>
+#include <cstdalign>
+#include <cstdbool>
+
+//Deprecated headers
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h<
+#include <string.h>
+#include <tgmath.h>
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
+
+
